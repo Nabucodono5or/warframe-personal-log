83b49049a2c683645ce44d662c47cdc3260bace4
Jefferson Abreu <pereirajefferson06@gmail.com>
2021-06-02 14:02:37 -0300

    terminado o estudo do commander library

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,33 @@
 #! /usr/bin/env node
 
-import { Command, Option } from 'commander';
+import { Command } from 'commander';
 
 const program = new Command();
 
 program.version('0.0.1');
 
-function myToUpperCase(dummyValue: string, previous: string): string {
-    const common: string = dummyValue.toUpperCase();
-    return common;
-}
+// program
+//     .command('clone <source> [destination]')
+//     .description('clone a repository into a newly created directory')
+//     .action((source, destination) => {
+//         console.log(`cloned from ${source} to destination ${destination}`);
+//     });
 
 program
-    .option('--no-sauce', 'Remove sauce')
-    .option('--cheese <flavour>', 'cheese flavour', 'mozzarella')
-    .option('--no-cheese', 'plain with no cheese')
-    .option('-d, --debug', 'output extra debugging')
-    .option('-s, --small', 'small pizza size')
-    .requiredOption('-p, --pizza-type <type>', 'flavour of pizza', myToUpperCase, '');
+    .version('0.1.0')
+    .command('user')
+    .arguments('<name>')
+    .option('-U, --upper', 'convert name to upper case')
+    .option('-l, --lower', 'convert name to lower case')
+    .description('test command', {
+        name: 'display simple name',
+    })
+    .action((name, options, command) => {
+        let inputName: string = name;
+        if (options.upper) inputName = name.toUpperCase();
+        if (options.lower) inputName = name.toLowerCase();
+        console.log('My name is ', inputName);
+        console.log('Executado por ', command);
+    });
 
 program.parse(process.argv);
-
-const options = program.opts();
-const sauceStr = options.sauce ? 'sauce' : 'no sauce';
-const cheeseStr = options.cheese === false ? 'no cheese' : `${options.cheese} cheese`;
-
-if (options.debug) console.log(options);
-console.log('pizza details:');
-
-if (options.small) console.log('- small pizza size');
-if (options.pizzaType) console.log(`- ${options.pizzaType}`);
-if (options.size) console.log(`- size ${options.size}`);
-
-console.log(`You ordered a pizza with ${sauceStr} and ${cheeseStr}`);
