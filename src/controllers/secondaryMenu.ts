0996b7b356dfadb3962321cc5fc8a68f67dcbd34
Jefferson Abreu <pereirajefferson06@gmail.com>
2021-07-15 14:01:42 -0300

    terminado o segundo menu

--- src/controllers/secondaryMenu.ts
+++ src/controllers/secondaryMenu.ts
@@ -1,20 +1,23 @@
 import inquirer from 'inquirer';
 import Warframe from '../schemas/warframe';
 
-// pegar os warframes do banco de dados retornando aqui
+const index = async () => {
+    const warframes = await Warframe.find({}, { name: 1 });
 
-const warframes = Warframe.find({}, { name: 1 });
+    const questions = [
+        {
+            type: 'list',
+            name: 'options',
+            message: 'Choose you warframe',
+            choices: warframes,
+        },
+    ];
 
-const questions = [
-    {
-        type: 'list',
-        name: 'options',
-        message: 'Choose you warframe',
-        choices: warframes,
-    },
-];
+    return questions;
+};
 
 const secondaryMenu = async (): Promise<void> => {
+    const questions = await index();
     const answers = await inquirer.prompt(questions);
     console.log(answers);
 };
